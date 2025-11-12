0809b11baba10c9464f8e95d0117741fa5b278ff
Saifullah Khan <92732640+saifkha09090@users.noreply.github.com>
2025-11-13 03:43:51 +0500

    2

--- notes-app-main/frontend/notes-app/vite.config.js
+++ notes-app-main/frontend/notes-app/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()], build: {
+  plugins: [react(), tailwindcss()],base: "./", build: {
     outDir: "dist",
   },
 })
