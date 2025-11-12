af0fecb864446bdcec5e3d0da2de9cdbc9ef206d
Saifullah Khan <92732640+saifkha09090@users.noreply.github.com>
2025-11-13 02:18:54 +0500

    Update index.js

--- notes-app-main/backend/api/index.js
+++ notes-app-main/backend/api/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173/", 
+  origin: "http://localhost:5173", 
   methods: ["GET", "POST", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
