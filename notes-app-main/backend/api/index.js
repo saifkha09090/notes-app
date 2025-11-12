25fb0e980201b3a4dab16a2de219b806e9c8296c
Saifullah Khan <92732640+saifkha09090@users.noreply.github.com>
2025-11-13 02:36:43 +0500

    Update index.js

--- notes-app-main/backend/api/index.js
+++ notes-app-main/backend/api/index.js
@@ -9,11 +9,13 @@ import noteRoutes from "./routes/noteRoutes.js";
 dotenv.config();
 const app = express();
 
-app.use(cors({
-  origin: process.env.CLIENT_URL, 
-  methods: ["GET", "POST", "PATCH", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/auth", authRoutes);
