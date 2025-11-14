58379840188d16055149d5ea871f1ef98ce58fd4
Saifullah Khan <92732640+saifkha09090@users.noreply.github.com>
2025-11-14 08:51:35 +0500

    Update authRoutes.js

--- notes-app-main/backend/routes/authRoutes.js
+++ notes-app-main/backend/routes/authRoutes.js
@@ -36,6 +36,7 @@ router.post("/login", async (req, res) => {
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 
     res.json({ token, user: { id: user._id, username: user.username, email: user.email } });
+    res.send("login")
   } catch (err) {
      console.error("Login Error:", err.message);
     res.status(500).json({ message: "Server error" });
