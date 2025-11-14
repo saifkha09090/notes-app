befac3536dc3d65b2c196323aeb3a56fa49c2734
Saifullah Khan <92732640+saifkha09090@users.noreply.github.com>
2025-11-14 09:14:25 +0500

    Update authRoutes.js

--- notes-app-main/backend/routes/authRoutes.js
+++ notes-app-main/backend/routes/authRoutes.js
@@ -2,7 +2,6 @@ import express from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
-
 const router = express.Router();
 
 router.post("/signup", async (req, res) => {
